82400a1ce23d338ee66df450ab7216d14c11d6f6
nicolencinas <nicolencinas@hotmail.com>
2019-11-06 19:19:27 -0300

    "Cambios en el codigo"

--- ARlearn/js/mensajeros copy.js
+++ ARlearn/js/mensajeros copy.js
@@ -2,10 +2,10 @@
 var destino=new Map();
 var origen=new Map();
 var driverspositions=new Map();
+var mensajeros=[];
 var pointPedidos=null;
 var map = null;
 var layerpedidos,layermensajeros=null;
-
 
 $(document).ready(function () 
 {
@@ -14,16 +14,12 @@ $(document).ready(function ()
 
 	layerpedidos=L.layerGroup().addTo(map);
 	layermensajeros=L.layerGroup().addTo(map);
-	$("#pedidos").empty();
-	$("#pedidos").append('<option>Buscando pedidos... </option>');
 
 	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
 		{
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 		maxZoom: 18
 		}).addTo(map);
-
-
 	
 	$.ajax(
 		{
@@ -35,23 +31,30 @@ $(document).ready(function ()
 
 			result = JSON.parse(JSON.stringify(json));		
 			
-			$("#pedidos").empty();
-			$("#pedidos").append('<option>Seleccione el numero de pedido... </option>');
 
 			for(var i=0;i<result['requests'].length;i++)
 			{
 				var id=result['requests'][i].id;
 				
-				
-				
 				$("#pedidos").append('<option value='+id+'>Pedido Nº '
 				+id+'</option>');
 				var od=result['requests'][i];
-				var ad=od.availableDrivers
+				var ad=od.availableDrivers;
 
 				origen.set(id,od.sender);
 				destino.set(id,od.receiver);
-				driverspositions.set(id,od.availableDrivers);
+				
+				
+				for (var i=0;i<ad.length;i++)
+				{
+					var mens=ad[i]['driver_id'];
+					var posiciones=ad[i]['position'];
+					driverspositions.set(mens,posiciones);
+				
+
+				}
+				console.log(driverspositions)
+				
 			}	
 
 
@@ -69,16 +72,13 @@ function obtenerMensajeros(elemento)
 
 
 			var urlpedido=elemento[elemento.selectedIndex].value;
-
-			var req=parseInt(urlpedido);
-			var olat=origen.get(req).lat
-			var olong=origen.get(req).lon;
-			var dlat=destino.get(req).lat;
-			var dlong=destino.get(req).lon;
+		
+			var olat=origen.get(parseInt(urlpedido)).lat
+			var olong=origen.get(parseInt(urlpedido)).lon;
+			var dlat=destino.get(parseInt(urlpedido)).lat;
+			var dlong=destino.get(parseInt(urlpedido)).lon;
 
 			
-			
-
 			
 
 		
@@ -92,69 +92,96 @@ function obtenerMensajeros(elemento)
 							maxZoom: 18
 						}).addTo(map);
 
+						L.control.scale().addTo(map);
 						layerpedidos.clearLayers();
 						layermensajeros.clearLayers();
 
-							
-						
+					
+
 						var op=L.marker([olat, olong], {draggable: true}).bindPopup("Origen pedido Nº "+urlpedido);
 						var dp=L.marker([dlat, dlong], {draggable: true}).bindPopup("Destino pedido Nº "+urlpedido);
 						
+
 						layerpedidos.addLayer(op);
 						layerpedidos.addLayer(dp);
+						
 
-					//	console.log(driverspositions)
-
-			
-			var candidates=driverspositions.get(req);
-			console.log(candidates);
+            			
+		
 			$("#mensajeros").empty();
-			$("#mensajeros").append('<option value="default">Seleccione un mensajero... </option>');
-
-			
-				for (var i=0;i<candidates.length;i++)
+			$("#mensajeros").append('<option value="default">Buscando mensajeros...</option>');
+			$.ajax(
 				{
-					var id1=candidates[i].driver_id;
-					var position=candidates[i].position;
-					var mlat=position.lat;
-					var mlong=position.lon;
-
-					var om=L.marker([mlat, mlong], {draggable: true}).bindPopup("Mensajero id "+id1)
-					
+					url : 'https://entregasya.herokuapp.com/api/requests/'+urlpedido,
 				
+					type : 'GET',
+					dataType : 'json',
+					success : function(json) 
+					{
 
-								$.ajax(
+					result = JSON.parse(JSON.stringify(json));		
+					drivers=result['request'].availableDrivers;
+				
+			
+					$("#mensajeros").empty();
+					$("#mensajeros").append('<option value="default">Seleccione un mensajero...</option>');
+
+						for (var i=0;i<drivers.length;i++)
+						{
+							var dr=driverspositions.get(drivers[i].driver_id);
+							var mlat=dr.lat;
+							var mlong=dr.lon;
+							var mp=
+
+
+
+							
+
+							$.ajax(
 								{
-									
-									url : 'https://entregasya.herokuapp.com/api/deliveryDrivers/'+id1,
+									url : 'https://entregasya.herokuapp.com/api/deliveryDrivers/'+drivers[i].driver_id,
 								
 									type : 'GET',
 									dataType : 'json',
 									success : function(json) 
 									{
+				
 									result = JSON.parse(JSON.stringify(json));		
-	
+									
 									var driver=result['driver'];
+									mensajeros.push(driver);
+									
 									var car=driver['car'].description
 									var carcolor=driver['car'].color;
-								
-									$("#mensajeros").append('<option value='+driver.id+'> -Nº '+driver.id+' '
+									
+									$("#mensajeros").append('<option value='+driver.id+'>'
 											+driver.name+' '+driver.surname+' ('+car +')  </option>');
 
-									
+									var me=L.marker([mlat, mlong], {draggable: true}).bindPopup("holis");
+									layermensajeros.addLayer(me);
 								},
 									error : function(jqXHR, status, error) 
 									{
 										alert('Disculpe, existió un problema');
 									}
-								})
+								});	
 
+
+						
 							
-								layermensajeros.addLayer(om);
+							
+							
+						}
+					
 								
-				}
-			
-			
+					
+						
+				},
+					error : function(jqXHR, status, error) 
+					{
+						alert('Disculpe, existió un problema');
+					}
+				});	
 		}
 
 
